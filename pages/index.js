61568246738bd3da373dcb573a49a456fa28a3fc
Taran Mand <tsmand@yahoo.com>
2020-12-20 12:32:44 +1100

    update intro

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Taran. I'm a software engineer.</p>
+        <p>Hello, I'm Taran. I'm a software engineer. I am specialized in JavaScript, ReactJS, NodeJS and AWS cloud platform.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
